7b7bb5531d564b803538f49f0da199c1f0b102a4
ggarabs <ggarabetti0607@gmail.com>
2026-01-17 14:02:16 -0300

    fix: include all domain-based routes in main ring handler

--- src/kanban/http/routes.clj
+++ src/kanban/http/routes.clj
@@ -1,13 +1,13 @@
 (ns kanban.http.routes
   (:require
    [reitit.ring :as ring]
-   [kanban.http.handlers.health :as health]
+   [kanban.http.routes.api :as api]
    [kanban.http.handlers.errors :as errors]))
 
 (def app-routes
   (ring/ring-handler
    (ring/router
-    [["/health" {:get health/handler}]])
+    [api/routes])
    (ring/create-default-handler
     {:not-found errors/not-found-handler
      :method-not-allowed errors/method-not-allowed-handler})))
